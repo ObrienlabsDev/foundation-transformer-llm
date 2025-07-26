f0f88db02c0b31e52cd582e5fd06bac5b7aa4823
Michael O'Brien <michael@obrienlabs.org>
2025-07-25 21:18:02 -0400

    #3 - java based LLM foundation model

---
+++ src/main/java/dev/obrienlabs/llm/FoundationTransformerLLMApplication.java
@@ -0,0 +1,13 @@
+package dev.obrienlabs.llm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoundationTransformerLLMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoundationTransformerLLMApplication.class, args);
+	}
+
+}
