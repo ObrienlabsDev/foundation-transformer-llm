7326763c037dd69cad1080cb5fea648d610f3b0d
Michael O'Brien <michael@obrienlabs.org>
2025-07-25 21:50:10 -0400

    #3 - java based LLM foundation model

---
+++ src/main/java/dev/obrienlabs/llm/prep/Tokenizer.java
@@ -0,0 +1,15 @@
+package dev.obrienlabs.llm.prep;
+
+public class Tokenizer {
+	
+	public void parse() {
+		
+	}
+
+	public static void main(String[] args) {
+		Tokenizer tokenizer = new Tokenizer();
+		tokenizer.parse();
+
+	}
+
+}
