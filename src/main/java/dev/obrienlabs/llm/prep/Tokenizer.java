f72610d3ff0fe14bda359ddeb2d0c8b5c64fa800
Michael OBrien <michael@obrienlabs.org>
2025-07-27 09:00:17 -0400

    #3 - start Scanner class

--- src/main/java/dev/obrienlabs/llm/prep/Tokenizer.java
+++ src/main/java/dev/obrienlabs/llm/prep/Tokenizer.java
@@ -1,8 +1,18 @@
 package dev.obrienlabs.llm.prep;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Tokenizer {
 	
 	public void parse() {
+		try {
+			File file = new File(getClass().getClassLoader().getResource("data/train.txt").getFile());
+			Scanner scanner = new Scanner(file);
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
 		
 	}
 
