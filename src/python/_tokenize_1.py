32432a6555e7f0c92c800f13deebfcaa2d67891a
Michael O'Brien <michael@obrienlabs.org>
2025-07-29 13:37:42 -0400

    #1 - LLM FS tokenize

--- src/python/_tokenize_1.py
+++ src/python/_tokenize_1.py
@@ -2,6 +2,7 @@
 # michaelobrien 20250728 using as a guide "build a large language model from scratch - Sebastian Raschka"
 
 import urllib.request
+import re
 
 # use a public dataset for initial testing
 url = ("https://raw.githubusercontent.com/rasbt/"
@@ -9,4 +10,14 @@ url = ("https://raw.githubusercontent.com/rasbt/"
        "the-verdict.txt")
 file_path = "data/the-verdict.txt"
 urllib.request.urlretrieve(url, file_path)
+
+# load downloaded file
+with open("data/the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+print("chars", len(raw_text))
+# tokenize on whitespace \s and periods/commans ,.
+result = re.split(r'(\s)|([.,])', raw_text)
+# truncate spaces (not recommended for code)
+#result = [token for token in result if token and not token.isspace()]
+print(result[:99]) # first 100 tokens
 
