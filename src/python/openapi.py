545ce90fa5e32d4672b0357db14b5c9f7ba8e195
Michael O'Brien <michael@obrienlabs.org>
2025-08-05 23:24:24 -0400

    #5 - openapi open models

---
+++ src/python/openapi.py
@@ -0,0 +1,35 @@
+
+# pip install -U transformers kernels torch 
+# pip install accelerate
+from transformers import pipeline
+import torch
+
+# for macOS
+print(torch.backends.mps.is_available())
+if torch.backends.mps.is_available():
+    mps_device = torch.device("mps")
+    x = torch.ones(1, device=mps_device)
+    print (x)
+else:
+    print ("MPS device not found.")
+
+
+#model_id = "openai/gpt-oss-120b"
+model_id = "openai/gpt-oss-20b"
+
+pipe = pipeline(
+    "text-generation",
+    model=model_id,
+    torch_dtype="auto",
+    device_map="cpu",#auto",
+)
+
+messages = [
+    {"role": "user", "content": "Explain quantum mechanics clearly and concisely."},
+]
+
+outputs = pipe(
+    messages,
+    max_new_tokens=256,
+)
+print(outputs[0]["generated_text"][-1])
