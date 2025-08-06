9c7f0ca2d3a0bb9ea9c9a59e635e1c6e5d105fcc
Michael O'Brien <michael@obrienlabs.org>
2025-08-05 23:37:05 -0400

    #5 - openapi open models

--- src/python/openapi.py
+++ src/python/openapi.py
@@ -22,8 +22,9 @@ model_id = "openai/gpt-oss-20b"
 pipe = pipeline(
     "text-generation",
     model=model_id,
+    device = "mps",
     torch_dtype="auto",
-    device_map="auto",
+    #device_map="auto",
 )
 
 messages = [
